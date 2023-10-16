a9ffed841d1ea9ce075ca6ef2b3bfbe9c5d3bcd2
Cristian Crețu <crisemcr@gmail.com>
2023-10-16 17:06:25 +0300

    feat: change icon (#1)
    * feat: change icon
    
    * feat: change logo on site
    
    * x

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,134 +1,166 @@
-import { ActionIcon, Box, Flex, Header, TextInput, Tooltip, useMantineTheme } from "@mantine/core";
-import { IconAdjustments, IconInfoCircle, IconShare2, IconWorld } from "@tabler/icons-react";
-import { EditChatModal } from "./EditChatModal";
-import { Chat, detaDB } from "../db";
-import { useEffect, useRef, useState } from "react";
-import { useChat, useChats } from "../hooks/contexts";
+import {
+ ActionIcon,
+ Box,
+ Flex,
+ Header,
+ TextInput,
+ Title,
+ Tooltip,
+ useMantineTheme,
+} from "@mantine/core";
 import { useDebouncedValue } from "@mantine/hooks";
 import { notifications } from "@mantine/notifications";
-import { ShareChatModal } from "./ShareChatModal";
-import { LogoText } from "./Logo";
+import {
+ IconAdjustments,
+ IconInfoCircle,
+ IconShare2,
+ IconWorld,
+} from "@tabler/icons-react";
 import { Link } from "@tanstack/react-location";
+import { useEffect, useRef, useState } from "react";
+import { Chat, detaDB } from "../db";
+import { useChat, useChats } from "../hooks/contexts";
+import { EditChatModal } from "./EditChatModal";
+import { ShareChatModal } from "./ShareChatModal";
 
-export function ChatHeader({ readOnly = false }: { readOnly?: boolean}) {
-    const [title, setTitle] = useState('')
-    const [debounced] = useDebouncedValue(title, 800);
-    const theme = useMantineTheme()
+export function ChatHeader({ readOnly = false }: { readOnly?: boolean }) {
+ const [title, setTitle] = useState("");
+ const [debounced] = useDebouncedValue(title, 800);
+ const theme = useMantineTheme();
 
-    const { chat, setChat } = useChat()
-    const { setChats } = useChats()
+ const { chat, setChat } = useChat();
+ const { setChats } = useChats();
 
-    useEffect(() => {
-        if (chat?.description) setTitle(chat.description)
-    }, [chat])
+ useEffect(() => {
+  if (chat?.description) setTitle(chat.description);
+ }, [chat]);
 
-    useEffect(() => {
-        const save = async () => {
-            if (!chat || !debounced || debounced === chat.description) return
-            await detaDB.chats.update({ description: title }, chat!.key)
-            setChat(current => ({ ...(current as Chat), description: title }))
-            setChats(current => (current || []).map(item => {
-                if (item.key === chat.key) {
-                    return { ...item, description: title };
-                }
-          
-                return item;
-            }))
+ useEffect(() => {
+  const save = async () => {
+   if (!chat || !debounced || debounced === chat.description) return;
+   await detaDB.chats.update({ description: title }, chat!.key);
+   setChat((current) => ({ ...(current as Chat), description: title }));
+   setChats((current) =>
+    (current || []).map((item) => {
+     if (item.key === chat.key) {
+      return { ...item, description: title };
+     }
 
-            notifications.show({
-                title: "Saved",
-                color: "green",
-                message: "Chat name updated.",
-            });
-        }
+     return item;
+    })
+   );
 
-        save()
-    }, [debounced])
+   notifications.show({
+    title: "Saved",
+    color: "green",
+    message: "Chat name updated.",
+   });
+  };
 
-    const ref = useRef<HTMLInputElement>(null);
+  save();
+ }, [debounced]);
 
-    useEffect(() => {
-        if (title && ref.current) {
-            ref.current.size = Math.max(title.length - 1, 8)
-        }
-    }, [title])
+ const ref = useRef<HTMLInputElement>(null);
 
-    return (
-        <Header height={60} p="xs" className="app-region-drag">
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-            <div>
-                {readOnly && (
-                    <Link to="/" style={{ display: 'block' }}>
-                        <LogoText
-                            style={{
-                                height: 22,
-                                color: theme.colors[theme.primaryColor][6],
-                                display: "block",
-                            }}
-                        />
-                    </Link>
-                )}
-            </div>
+ useEffect(() => {
+  if (title && ref.current) {
+   ref.current.size = Math.max(title.length - 1, 8);
+  }
+ }, [title]);
 
-            <TextInput
-                id="chat-name"
-                ref={ref}
-                placeholder="Chat Name"
-                variant="unstyled"
-                value={title}
-                onChange={(event) => setTitle(event.currentTarget.value)}
-                styles={{ input: { fontSize: '1rem', textAlign: 'center' } }}
-                autoComplete="off"
-                data-lpignore="true"
-                data-form-type="other"
-                readOnly={readOnly}
-            />
+ return (
+  <Header height={60} p="xs" className="app-region-drag">
+   <Box
+    sx={{
+     display: "flex",
+     alignItems: "center",
+     justifyContent: "space-between",
+    }}
+   >
+    <div>
+     {readOnly && (
+      <Link to="/" style={{ display: "block" }}>
+       <Title
+        order={3}
+        style={{
+         color: theme.colors[theme.primaryColor][6],
+        }}
+       >
+        Dialogue
+       </Title>
+      </Link>
+     )}
+    </div>
 
-            <Flex
-                align="center"
-                sx={(theme) => ({
-                    [theme.fn.smallerThan('md')]: {
-                        marginRight: readOnly ? '' : '2.5rem',
-                        paddingBottom: 3,
-                    },
-                })}
-            >
-                <ShareChatModal chat={chat!} readOnly={readOnly}>
-                    <Tooltip label={chat!.shared && !readOnly ? "Shared Chat" : "Share Chat"}>
-                        <ActionIcon size="xl" loaderProps={{ size: 20 }} pos="relative">
-                            {chat!.shared && !readOnly ? (
-                                <>
-                                    <IconWorld size={20} />
-                                    <Box w={10} h={10} sx={(theme) => ({ background: theme.colors.blue[6], borderRadius: '100%' })} pos="absolute" top={8} right={8}></Box>
-                                </>
-                            ) : (
-                                <IconShare2 size={20} />
-                            )}
-                        </ActionIcon>
-                    </Tooltip>
-                </ShareChatModal>
+    <TextInput
+     id="chat-name"
+     ref={ref}
+     placeholder="Chat Name"
+     variant="unstyled"
+     value={title}
+     onChange={(event) => setTitle(event.currentTarget.value)}
+     styles={{ input: { fontSize: "1rem", textAlign: "center" } }}
+     autoComplete="off"
+     data-lpignore="true"
+     data-form-type="other"
+     readOnly={readOnly}
+    />
 
-                {!readOnly ? (
-                    <>
-                        <EditChatModal chat={chat!}>
-                            <Tooltip label="Chat Settings">
-                                <ActionIcon size="xl">
-                                    <IconAdjustments size={20} />
-                                </ActionIcon>
-                            </Tooltip>
-                        </EditChatModal>
-                    </>
-                ) : (
-                    <Tooltip label="What is this?">
-                        <a href="https://deta.space/discovery/@deta/dialogue" target="_blank">
-                            <ActionIcon size="xl">
-                                <IconInfoCircle size={20} />
-                            </ActionIcon>
-                        </a>
-                    </Tooltip>
-                )}
-            </Flex>
-        </Box>
-    </Header>
-    )
+    <Flex
+     align="center"
+     sx={(theme) => ({
+      [theme.fn.smallerThan("md")]: {
+       marginRight: readOnly ? "" : "2.5rem",
+       paddingBottom: 3,
+      },
+     })}
+    >
+     <ShareChatModal chat={chat!} readOnly={readOnly}>
+      <Tooltip label={chat!.shared && !readOnly ? "Shared Chat" : "Share Chat"}>
+       <ActionIcon size="xl" loaderProps={{ size: 20 }} pos="relative">
+        {chat!.shared && !readOnly ? (
+         <>
+          <IconWorld size={20} />
+          <Box
+           w={10}
+           h={10}
+           sx={(theme) => ({
+            background: theme.colors.blue[6],
+            borderRadius: "100%",
+           })}
+           pos="absolute"
+           top={8}
+           right={8}
+          ></Box>
+         </>
+        ) : (
+         <IconShare2 size={20} />
+        )}
+       </ActionIcon>
+      </Tooltip>
+     </ShareChatModal>
+
+     {!readOnly ? (
+      <>
+       <EditChatModal chat={chat!}>
+        <Tooltip label="Chat Settings">
+         <ActionIcon size="xl">
+          <IconAdjustments size={20} />
+         </ActionIcon>
+        </Tooltip>
+       </EditChatModal>
+      </>
+     ) : (
+      <Tooltip label="What is this?">
+       <a href="https://deta.space/discovery/@deta/dialogue" target="_blank">
+        <ActionIcon size="xl">
+         <IconInfoCircle size={20} />
+        </ActionIcon>
+       </a>
+      </Tooltip>
+     )}
+    </Flex>
+   </Box>
+  </Header>
+ );
 }
--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,19 +1,24 @@
 import {
-  ActionIcon,
-  Box,
-  Card,
-  Code,
-  Collapse,
-  CopyButton,
-  Flex,
-  Table,
-  Text,
-  ThemeIcon,
-  Tooltip,
-  useMantineTheme,
+ ActionIcon,
+ Box,
+ Card,
+ Code,
+ Collapse,
+ CopyButton,
+ Flex,
+ Table,
+ Text,
+ ThemeIcon,
+ Tooltip,
+ useMantineTheme,
 } from "@mantine/core";
 import { useClickOutside } from "@mantine/hooks";
-import { IconArrowForward, IconCopy, IconTrash, IconUser } from "@tabler/icons-react";
+import {
+ IconArrowForward,
+ IconCopy,
+ IconTrash,
+ IconUser,
+} from "@tabler/icons-react";
 import { useEffect, useMemo, useRef, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -25,145 +30,166 @@ import { notifications } from "@mantine/notifications";
 import { useSpaceAppActions } from "../hooks/contexts";
 import { SelectIntegrationModal } from "./SelectIntegrationModal";
 
-export function MessageItem({ message, readOnly = false, onDeleted, handleUseMessage }: { message: Message, readOnly?: boolean, onDeleted?: (key: string) => void, handleUseMessage?: (key: string) => void }) {
-  const wordCount = useMemo(() => {
-    var matches = message.content.match(/[\w\d\’\'-\(\)]+/gi);
-    return matches ? matches.length : 0;
-  }, [message.content]);
+export function MessageItem({
+ message,
+ readOnly = false,
+ onDeleted,
+ handleUseMessage,
+}: {
+ message: Message;
+ readOnly?: boolean;
+ onDeleted?: (key: string) => void;
+ handleUseMessage?: (key: string) => void;
+}) {
+ const wordCount = useMemo(() => {
+  var matches = message.content.match(/[\w\d\’\'-\(\)]+/gi);
+  return matches ? matches.length : 0;
+ }, [message.content]);
 
-  const ref = useClickOutside(() => setExpanded(false));
-  const theme = useMantineTheme();
-  const markdownRef = useRef<HTMLDivElement>(null)
+ const ref = useClickOutside(() => setExpanded(false));
+ const theme = useMantineTheme();
+ const markdownRef = useRef<HTMLDivElement>(null);
 
-  const { spaceAppActions } = useSpaceAppActions()
-  
-  const [isExpanded, setExpanded] = useState(false);
-  const [html, setHtml] = useState('')
+ const { spaceAppActions } = useSpaceAppActions();
 
-  useEffect(() => {
-    setHtml(markdownRef.current?.innerHTML || '')
-  }, [ref, message])
+ const [isExpanded, setExpanded] = useState(false);
+ const [html, setHtml] = useState("");
 
-  const handleDelete = async () => {
-    await detaDB.messages.delete(message.key);
+ useEffect(() => {
+  setHtml(markdownRef.current?.innerHTML || "");
+ }, [ref, message]);
 
-    if (onDeleted) onDeleted(message.key)
+ const handleDelete = async () => {
+  await detaDB.messages.delete(message.key);
 
-    setExpanded(false);
+  if (onDeleted) onDeleted(message.key);
 
-    notifications.show({
-      title: "Deleted",
-      color: "green",
-      message: "Message deleted.",
-    });
-  }
+  setExpanded(false);
 
-  return (
-    <Card
-      ref={ref}
-      withBorder
-      onClick={() => setExpanded(true)}
-      style={{ cursor: 'pointer' }}
-      sx={{
-        '&:hover': {
-          border: '1px solid #acacac'
-        }
+  notifications.show({
+   title: "Deleted",
+   color: "green",
+   message: "Message deleted.",
+  });
+ };
+
+ return (
+  <Card
+   ref={ref}
+   withBorder
+   onClick={() => setExpanded(true)}
+   style={{ cursor: "pointer" }}
+   sx={{
+    "&:hover": {
+     border: "1px solid #acacac",
+    },
+   }}
+  >
+   <Flex gap="sm">
+    {message.role === "user" && (
+     <ThemeIcon color="gray" size="lg">
+      <IconUser size={20} />
+     </ThemeIcon>
+    )}
+    {message.role === "assistant" && <LogoIcon w={32} />}
+    <Box sx={{ flex: 1, width: 0 }} className="markdown" ref={markdownRef}>
+     <ReactMarkdown
+      children={message.content}
+      remarkPlugins={[remarkGfm]}
+      components={{
+       table: ({ node, ...props }) => (
+        <Table verticalSpacing="sm" highlightOnHover {...props} />
+       ),
+       code: ({ node, inline, ...props }) =>
+        inline ? (
+         <Code {...props} />
+        ) : (
+         <Box sx={{ position: "relative" }}>
+          <Code block {...props} />
+          <CopyButton value={String(props.children)}>
+           {({ copied, copy }) => (
+            <Tooltip label={copied ? "Copied" : "Copy"} position="left">
+             <ActionIcon
+              sx={{ position: "absolute", top: 4, right: 4 }}
+              onClick={copy}
+             >
+              <IconCopy opacity={0.4} size={20} />
+             </ActionIcon>
+            </Tooltip>
+           )}
+          </CopyButton>
+         </Box>
+        ),
       }}
+     />
+    </Box>
+   </Flex>
+
+   <Collapse in={isExpanded}>
+    <Box
+     style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginTop: 10,
+     }}
     >
-      <Flex gap="sm">
-        {message.role === "user" && (
-          <ThemeIcon color="gray" size="lg">
-            <IconUser size={20} />
-          </ThemeIcon>
-        )}
-        {message.role === "assistant" && <LogoIcon style={{ height: 32 }} color1={theme.colors[theme.primaryColor][3]} color2={theme.colors[theme.primaryColor][7]} />}
-        <Box sx={{ flex: 1, width: 0 }} className="markdown" ref={markdownRef}>
-          <ReactMarkdown
-            children={message.content}
-            remarkPlugins={[remarkGfm]}
-            components={{
-              table: ({ node, ...props }) => (
-                <Table verticalSpacing="sm" highlightOnHover {...props} />
-              ),
-              code: ({ node, inline, ...props }) =>
-                inline ? (
-                  <Code {...props} />
-                ) : (
-                  <Box sx={{ position: "relative" }}>
-                    <Code block {...props} />
-                    <CopyButton value={String(props.children)}>
-                      {({ copied, copy }) => (
-                        <Tooltip
-                          label={copied ? "Copied" : "Copy"}
-                          position="left"
-                        >
-                          <ActionIcon
-                            sx={{ position: "absolute", top: 4, right: 4 }}
-                            onClick={copy}
-                          >
-                            <IconCopy opacity={0.4} size={20} />
-                          </ActionIcon>
-                        </Tooltip>
-                      )}
-                    </CopyButton>
-                  </Box>
-                ),
-            }}
+     <Box>
+      <Text size="sm" color="dimmed">
+       {wordCount} words
+      </Text>
+     </Box>
+
+     <Box style={{ display: "flex" }}>
+      {spaceAppActions !== null && (
+       <SelectIntegrationModal contentHtml={html} contentText={message.content}>
+        <Tooltip label="Send to Space" position="top">
+         <ActionIcon>
+          {/* <IconBolt opacity={0.5} size={20} /> */}
+          <img
+           src="/assets/integrations/deta.png"
+           height={20}
+           style={{ opacity: 0.75 }}
           />
-        </Box>
-      </Flex>
-      
-      <Collapse in={isExpanded}>
-        <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: 10 }}>
-          <Box>
-            <Text size="sm" color="dimmed">
-              {wordCount} words
-            </Text>
-          </Box>
+         </ActionIcon>
+        </Tooltip>
+       </SelectIntegrationModal>
+      )}
 
-          <Box style={{ display: 'flex' }}>
-            {spaceAppActions !== null && (
-                <SelectIntegrationModal contentHtml={html} contentText={message.content}>
-                  <Tooltip label="Send to Space" position="top">
-                      <ActionIcon >
-                        {/* <IconBolt opacity={0.5} size={20} /> */}
-                        <img src="/assets/integrations/deta.png" height={20} style={{ opacity: 0.75 }} />
-                      </ActionIcon>
-                  </Tooltip>
-                </SelectIntegrationModal>
-            )}
+      {!readOnly && handleUseMessage && (
+       <>
+        <CreatePromptModal content={message.content || ""} />
+        <Tooltip label="Re-use Message" position="top">
+         <ActionIcon onClick={() => handleUseMessage(message.content)}>
+          <IconArrowForward opacity={0.5} size={20} />
+         </ActionIcon>
+        </Tooltip>
+       </>
+      )}
 
-            {!readOnly && handleUseMessage && (
-              <>
-                <CreatePromptModal content={message.content || ''} />
-                <Tooltip label="Re-use Message" position="top">
-                  <ActionIcon onClick={() => handleUseMessage(message.content)}>
-                    <IconArrowForward opacity={0.5} size={20} />
-                  </ActionIcon>
-                </Tooltip>
-              </>
-            )}
+      <CopyButton value={message.content}>
+       {({ copied, copy }) => (
+        <Tooltip
+         label={copied ? "Copied Message" : "Copy Message"}
+         position="top"
+        >
+         <ActionIcon onClick={copy}>
+          <IconCopy opacity={0.5} size={20} />
+         </ActionIcon>
+        </Tooltip>
+       )}
+      </CopyButton>
 
-            <CopyButton value={message.content}>
-              {({ copied, copy }) => (
-                <Tooltip label={copied ? "Copied Message" : "Copy Message"} position="top">
-                  <ActionIcon onClick={copy}>
-                    <IconCopy opacity={0.5} size={20} />
-                  </ActionIcon>
-                </Tooltip>
-              )}
-            </CopyButton>
-
-            {!readOnly && (
-              <Tooltip label="Delete Message" position="top">
-                <ActionIcon onClick={handleDelete}>
-                  <IconTrash opacity={0.5} size={20} />
-                </ActionIcon>
-              </Tooltip>
-            )}
-          </Box>
-        </Box>
-      </Collapse>
-    </Card>
-  );
+      {!readOnly && (
+       <Tooltip label="Delete Message" position="top">
+        <ActionIcon onClick={handleDelete}>
+         <IconTrash opacity={0.5} size={20} />
+        </ActionIcon>
+       </Tooltip>
+      )}
+     </Box>
+    </Box>
+   </Collapse>
+  </Card>
+ );
 }
